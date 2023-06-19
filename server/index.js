b561ba3955036929bf31ced80c80e0c433b693e1
Aldo Pradana Ariando <87022323+aldopradana3@users.noreply.github.com>
2023-06-19 18:36:12 +0700

    Update index.js
    hapus cookie

--- server/index.js
+++ server/index.js
@@ -46,9 +46,6 @@ app.use(
         secret: process.env.SESSION_SECRET,
         resave: false,
         saveUninitialized: true,
-        cookie: {
-            secure: false,
-        },
     }),
 )
 
