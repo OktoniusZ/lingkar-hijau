192fefdea237da8008de6f95f363dd7278893daf
Kuncoro Ariadi <119470883+kuncoroariadi12@users.noreply.github.com>
2023-06-19 20:31:48 +0700

    Add configuration

--- webpack.common.js
+++ webpack.common.js
@@ -43,11 +43,11 @@ module.exports = {
         {
           from: path.resolve(__dirname, 'src/public/'),
           to: path.resolve(__dirname, 'dist/'),
-          globOptions: {
-            // CopyWebpackPlugin mengabaikan
-            // berkas yang berada di dalam folder images
-            ignore: ['**/images/**'],
-          },
+          // globOptions: {
+          //   // CopyWebpackPlugin mengabaikan
+          //   // berkas yang berada di dalam folder images
+          //   ignore: ['**/images/**'],
+          // },
         },
       ],
     }),
